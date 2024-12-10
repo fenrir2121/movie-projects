51ed0954b0e92305ed2441734cd383b1f8966725
AdrianDab/ <129049292+fenrir2121@users.noreply.github.com>
2024-12-10 11:38:31 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   return (
     <div className="movie-container">
       <div className="movie-heading">
-        <h1>Moviesssssssssssssss</h1>
+        <h1>Movies</h1>
       </div>
 
       <div className="searchbar-row">
